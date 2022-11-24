776fa52b6ac781b8e3bb84d33cb5623db4f24f71
Suhail Akhtar <suhailakhtar039@gmail.com>
2022-11-24 22:55:07 +0530

    Decode ways

---
+++ src/practice/DecodeWays.java
@@ -0,0 +1,25 @@
+package practice;
+
+import java.util.Scanner;
+
+public class DecodeWays {
+    private static int solve(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] += dp[i + 1];
+                if (i + 1 < n && (s.charAt(i) == '1' || s.charAt(i) == '2' && s.charAt(i + 1) < '7'))
+                    dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(solve(s));
+    }
+}
