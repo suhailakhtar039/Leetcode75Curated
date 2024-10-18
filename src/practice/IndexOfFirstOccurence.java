aa9cfb69763bd0cc3a301ee7eeb982c3ba842a3b
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-10-18 22:55:22 +0530

    index of first occurence

---
+++ src/practice/IndexOfFirstOccurence.java
@@ -0,0 +1,24 @@
+package practice;
+
+public class IndexOfFirstOccurence {
+    public static void main(String[] args) {
+        String haystack = "leetcode";
+        String needle = "tco";
+
+
+        int n = haystack.length(), j = needle.length();
+        int index = -1;
+        for(int i = 0; i<n; i++){
+            int k = 0, l = i;
+            while(l < n && k < j && needle.charAt(k) == haystack.charAt(l)){
+                k++;
+                l++;
+            }
+            if(k == needle.length()){
+                index = i;
+                break;
+            }
+        }
+        System.out.println("index = " + index);
+    }
+}
