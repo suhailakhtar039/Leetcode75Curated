0e71de139e3777104d208f4381a9b43915fa85b9
Suhail Akhtar <suhailakhtar039@gmail.com>
2024-11-02 23:47:05 +0530

    kth largest element

---
+++ src/practice/graph/KthLargestElement.java
@@ -0,0 +1,21 @@
+package practice.graph;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 3;
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = nums.length;
+        for(int i =0; i<k; i++)pq.add(nums[i]);
+        for(int i = k; i<n; i++){
+            pq.add(nums[i]);
+            pq.poll();
+        }
+        System.out.println(pq.peek());
+
+    }
+}
